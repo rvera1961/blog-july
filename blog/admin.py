eaa84cccbe3f4ae63a7f103af220a4f94fcd34c2
unknown <raulvera@hotmail.com>
2025-08-18 20:59:42 -0400

    blog app added

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at', 'author')
+
+# Register your models here.
+admin.site.register(Post, PostAdmin)
