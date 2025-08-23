eabd5e04639b160170c154440e7aefffd5712d7f
unknown <raulvera@hotmail.com>
2025-08-22 22:03:58 -0400

    post detail add

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
+
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
     template_name = "posts_list.html"
+
+# Detail view for a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post-detail.html"
